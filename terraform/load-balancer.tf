e7c7013ba520265bb0fd477fe3b2a94910ce1f12
Sai Charan <charankuppa@gmail.com>
2021-05-15 23:37:01 +0200

    tf - add load balancer

---
+++ terraform/load-balancer.tf
@@ -0,0 +1,26 @@
+resource "aws_alb" "application_load_balancer" {
+  name               = "tf-lb"
+  load_balancer_type = "application"
+  subnets = [
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}",
+    "${aws_default_subnet.default_subnet_c.id}"
+  ]
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
