c4d9b7a4ff3e14b3321fa0885e683180c220e42f
Sai Charan <charankuppa@gmail.com>
2021-05-15 23:30:41 +0200

    tf - add arn role

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -21,9 +21,9 @@ resource "aws_ecs_task_definition" "tf_ecs_task" {
     }
   ]
   DEFINITION
-  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
-  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
-  memory                   = 512         # Specifying the memory our container requires
-  cpu                      = 256         # Specifying the CPU our container requires
-  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+  requires_compatibilities = ["FARGATE"] 
+  network_mode             = "awsvpc"    
+  memory                   = 512         
+  cpu                      = 256         
+  execution_role_arn       = "arn:aws:iam::417834101719:role/ecsTaskExecutionRole"
 }
