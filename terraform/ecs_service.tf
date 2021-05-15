a98823880ce13c5a0e1c1b96f8961998c0a2e36e
Sai Charan <charankuppa@gmail.com>
2021-05-15 23:41:48 +0200

    tf - add load balancer to the ecs service

--- terraform/ecs_service.tf
+++ terraform/ecs_service.tf
@@ -5,6 +5,11 @@ resource "aws_ecs_service" "tf-ecs-service" {
   launch_type     = "FARGATE"
   desired_count   = 1
 
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}"
+    container_name   = "${aws_ecs_task_definition.tf_ecs_task.family}"
+    container_port   = 8080
+  }
   network_configuration {
     subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
     assign_public_ip = true
