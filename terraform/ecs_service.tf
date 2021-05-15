6147f433093cda7b377c51c700c015b78a98b05e
Sai Charan <charankuppa@gmail.com>
2021-05-15 23:42:43 +0200

    tf - add ingress and egress to the security group

--- terraform/ecs_service.tf
+++ terraform/ecs_service.tf
@@ -15,3 +15,19 @@ resource "aws_ecs_service" "tf-ecs-service" {
     assign_public_ip = true
   }
 }
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
